da476091e5e698c962fa6fed5f30238280f18f5c
Seeun Oh <so37@maroon09.cs.calvin.edu>
2018-12-07 21:00:41 -0500

    About Page

--- app/scripts/aboutPage/aboutMain.js
+++ app/scripts/aboutPage/aboutMain.js
@@ -1,13 +1,21 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
-class AboutMain extends PureComponent {
+class AboutMain extends Component {
 
   render() {
     return (
-      <div>About Page</div>
+      <div>About Page
+      <div class="column">
+      <p>aboutSection</p>
+      <p>CommitteeSection</p>
+      </div>
+      <div class="column">
+      <p>VisionSection</p>
+      <p>AlumniSection</p>
+      </div> 
+      </div>
     );
-  }
-
+    }
 }
 
 export default AboutMain;
